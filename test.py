23b22502b2182abfbc946d00531a9eb6c66dec13
staiyeba <staiyeba@github.com>
2016-09-21 11:18:05 +0200

    updating test.py

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-import os
+
 import subprocess
+import os
 
-#file='/home/syeda/vagrant/varnish-migrate3to4/files/testvcl.vcl'
-subprocess.call(["python varnish3to4 -o testvclv4 testvcl.vcl"], shell=True)
+#filename='/home/syeda/vagrant/varnish-migrate3to4/files/testvcl.vcl'
 
-#os.system('python varnish3to4 -o testvclv4 file')
+
+#call_cmd = 'python varnish3to4 -o {0} {1}'.format("blaaa", filename)
+
+for filename in os.listdir('/home/syeda/vagrant/varnish-migrate3to4/files'):
+    if filename.endswith(".vcl"):
+        print filename
+        outfile_name = '{0}{1}'.format(filename,".v4")
+        print outfile_name
+        call_cmd = 'python varnish3to4 -o {0} {1}'.format(outfile_name, filename)
+        print call_cmd
+        subprocess.call(call_cmd, shell=True)
